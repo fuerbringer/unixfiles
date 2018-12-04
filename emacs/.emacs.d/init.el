5b9e562168a9307618de2cf2c27d39a7575d9fb5
fuerbringer <severin@protonmail.ch>
2018-12-04 21:46:49 +0100

    Remove font and themes from emacs conf.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -92,12 +92,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (leuven)))
  '(org-agenda-files (quote ("~/org")))
  '(package-selected-packages (quote (evil))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#FFFFFF" :foreground "#333333" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "CTDB" :family "DejaVu Sans Mono")))))
